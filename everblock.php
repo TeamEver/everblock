2baea3046851efe4e0663d85d055ca5173d93827
cchalamon <cyril.chalamon@celaneo.com>
2023-03-05 19:23:28 +0100

    lvl up

--- everblock.php
+++ everblock.php
@@ -32,7 +32,7 @@ class Everblock extends Module
     {
         $this->name = 'everblock';
         $this->tab = 'front_office_features';
-        $this->version = '3.4.1';
+        $this->version = '3.4.2';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
