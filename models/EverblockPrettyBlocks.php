ac76b2bd2c9aeda9cc0847cb5a3893a7ef4b153a
Cyssoo <cyrilchalamon@gmail.com>
2025-06-12 19:37:23 +0200

    Add link list prettyblock

--- models/EverblockPrettyBlocks.php
+++ models/EverblockPrettyBlocks.php
@@ -133,6 +133,7 @@ class EverblockPrettyBlocks extends ObjectModel
             $reassuranceTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_reassurance.tpl';
             $ctaTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_cta.tpl';
             $sharerTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_sharer.tpl';
+            $linkListTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_link_list.tpl';
             $defaultLogo = Tools::getHttpHost(true) . __PS_BASE_URI__ . 'modules/' . $module->name . '/logo.png';
             $blocks = [];
             $allShortcodes = EverblockShortcode::getAllShortcodes(
@@ -2146,6 +2147,84 @@ class EverblockPrettyBlocks extends ObjectModel
                     ],
                 ],
             ];
+            $blocks[] = [
+                'name' => $module->l('Links list'),
+                'description' => $module->l('Display a list of links'),
+                'code' => 'everblock_link_list',
+                'tab' => 'general',
+                'icon_path' => $defaultLogo,
+                'need_reload' => true,
+                'templates' => [
+                    'default' => $linkListTemplate,
+                ],
+                'repeater' => [
+                    'name' => 'Link',
+                    'nameFrom' => 'name',
+                    'groups' => [
+                        'name' => [
+                            'type' => 'text',
+                            'label' => $module->l('Link text'),
+                            'default' => $module->l('My link'),
+                        ],
+                        'url' => [
+                            'type' => 'text',
+                            'label' => $module->l('Link URL'),
+                            'default' => '#',
+                        ],
+                        'target_blank' => [
+                            'type' => 'checkbox',
+                            'label' => $module->l('Open in new tab'),
+                            'default' => '0',
+                        ],
+                        'css_class' => [
+                            'type' => 'text',
+                            'label' => $module->l('Custom CSS class'),
+                            'default' => '',
+                        ],
+                        'padding_left' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding left (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_right' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding right (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_top' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding top (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_bottom' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding bottom (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_left' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin left (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_right' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin right (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_top' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin top (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_bottom' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin bottom (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                    ],
+                ],
+            ];
+
             $blocks[] = [
                 'name' => $module->l('Page sharer'),
                 'description' => $module->l('Display social share buttons'),
---
+++ views/templates/hook/prettyblocks/prettyblock_link_list.tpl
@@ -0,0 +1,45 @@
+{*
+ * 2019-2025 Team Ever
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ *  @author    Team Ever <https://www.team-ever.com/>
+ *  @copyright 2019-2025 Team Ever
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*}
+<div id="block-{$block.id_prettyblocks}" class="{if $block.settings.default.force_full_width}w-100 px-0 mx-0{elseif $block.settings.default.container}container{/if}">
+  {if $block.settings.default.force_full_width}
+    <div class="row gx-0 no-gutters">
+  {elseif $block.settings.default.container}
+    <div class="row">
+  {/if}
+  {if isset($block.states) && $block.states}
+    <ul class="list-unstyled">
+      {foreach from=$block.states item=state key=key}
+        <li class="{if $state.css_class}{$state.css_class|escape:'htmlall'}{/if}" style="
+          {if $state.padding_left}padding-left:{$state.padding_left};{/if}
+          {if $state.padding_right}padding-right:{$state.padding_right};{/if}
+          {if $state.padding_top}padding-top:{$state.padding_top};{/if}
+          {if $state.padding_bottom}padding-bottom:{$state.padding_bottom};{/if}
+          {if $state.margin_left}margin-left:{$state.margin_left};{/if}
+          {if $state.margin_right}margin-right:{$state.margin_right};{/if}
+          {if $state.margin_top}margin-top:{$state.margin_top};{/if}
+          {if $state.margin_bottom}margin-bottom:{$state.margin_bottom};{/if}">
+          <a href="{$state.url|escape:'htmlall'}" class="text-decoration-none link-secondary"{if $state.target_blank} target="_blank"{/if}>{$state.name|escape:'htmlall'}</a>
+        </li>
+      {/foreach}
+    </ul>
+  {/if}
+  {if $block.settings.default.force_full_width || $block.settings.default.container}
+    </div>
+  {/if}
+</div>
+<!-- /Module Ever Block -->
