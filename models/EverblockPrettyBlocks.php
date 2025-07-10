5e4c9823167fd54344ff1829d3cb20991bfac367
Cyssoo <cyrilchalamon@gmail.com>
2025-07-10 19:27:37 +0200

    Add Masonry gallery Prettyblock

--- models/EverblockPrettyBlocks.php
+++ models/EverblockPrettyBlocks.php
@@ -124,6 +124,7 @@ class EverblockPrettyBlocks extends ObjectModel
             $tabTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_tab.tpl';
             $dividerTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_divider.tpl';
             $galleryTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_gallery.tpl';
+            $masonryGalleryTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_masonry_gallery.tpl';
             $testimonialTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_testimonial.tpl';
             $parallaxTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_parallax.tpl';
             $overlayTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_overlay.tpl';
@@ -1693,6 +1694,96 @@ class EverblockPrettyBlocks extends ObjectModel
                             'default' => [
                                 'url' => '',
                             ],
+                        ],
+                        'css_class' => [
+                            'type' => 'text',
+                            'label' => $module->l('Custom CSS class'),
+                            'default' => '',
+                        ],
+                        'padding_left' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding left (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_right' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding right (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_top' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding top (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_bottom' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding bottom (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_left' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin left (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_right' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin right (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_top' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin top (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_bottom' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin bottom (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                    ],
+                ],
+            ];
+            $blocks[] = [
+                'name' => $module->l('Masonry gallery'),
+                'description' => $module->l('Show masonry style image gallery'),
+                'code' => 'everblock_masonry_gallery',
+                'tab' => 'general',
+                'icon_path' => $defaultLogo,
+                'need_reload' => true,
+                'templates' => [
+                    'default' => $masonryGalleryTemplate,
+                ],
+                'repeater' => [
+                    'name' => 'Image name',
+                    'nameFrom' => 'name',
+                    'groups' => [
+                        'name' => [
+                            'type' => 'text',
+                            'label' => 'Image title',
+                            'default' => Configuration::get('PS_SHOP_NAME'),
+                        ],
+                        'image' => [
+                            'type' => 'fileupload',
+                            'label' => 'Image',
+                            'path' => '$/modules/' . $module->name . '/views/img/prettyblocks/',
+                            'default' => [
+                                'url' => '',
+                            ],
+                        ],
+                        'image_width' => [
+                            'type' => 'text',
+                            'label' => $module->l('Image width (e.g., 100px or 50%)'),
+                            'default' => 'auto',
+                        ],
+                        'image_height' => [
+                            'type' => 'text',
+                            'label' => $module->l('Image height (e.g., 100px)'),
+                            'default' => 'auto',
+                        ],
+                        'alt' => [
+                            'type' => 'text',
+                            'label' =>  $module->l('alt attribute'),
+                            'default' => $module->l('My alt attribute')
                         ],
                         'css_class' => [
                             'type' => 'text',
