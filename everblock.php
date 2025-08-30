eaced32aaed4627bbdc8de109b2c9d1eb0cf7a26
Cyssoo <cyrilchalamon@gmail.com>
2025-08-30 10:32:05 +0200

    Bump module version to 7.3.2

--- everblock.php
+++ everblock.php
@@ -64,7 +64,7 @@ class Everblock extends Module
     {
         $this->name = 'everblock';
         $this->tab = 'front_office_features';
-        $this->version = '7.3.1';
+        $this->version = '7.3.2';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
