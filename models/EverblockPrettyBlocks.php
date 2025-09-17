f9a60189b35617dda955f18babb54a464387d30a
Cyssoo <cyrilchalamon@gmail.com>
2025-09-17 19:33:55 +0200

    Add slot machine prettyblock with front controller and assets

--- models/EverblockPrettyBlocks.php
+++ models/EverblockPrettyBlocks.php
@@ -94,6 +94,7 @@ class EverblockPrettyBlocks extends ObjectModel
             $specialEventTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_special_event.tpl';
             $wheelTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_wheel_of_fortune.tpl';
             $mysteryBoxesTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_mystery_boxes.tpl';
+            $slotMachineTemplate = 'module:' . $module->name . '/views/templates/hook/prettyblocks/prettyblock_slot_machine.tpl';
             $defaultLogo = Tools::getHttpHost(true) . __PS_BASE_URI__ . 'modules/' . $module->name . '/logo.png';
             $blocks = [];
             $allShortcodes = EverblockShortcode::getAllShortcodes(
@@ -5103,6 +5104,183 @@ class EverblockPrettyBlocks extends ObjectModel
                             'type' => 'checkbox',
                             'label' => $module->l('Winning box'),
                             'default' => false,
+                        ],
+                    ],
+                ],
+            ];
+            $blocks[] = [
+                'name' => $module->l('Slot machine'),
+                'description' => $module->l('Interactive slot machine mini game'),
+                'code' => 'everblock_slot_machine',
+                'tab' => 'general',
+                'icon_path' => $defaultLogo,
+                'need_reload' => true,
+                'templates' => [
+                    'default' => $slotMachineTemplate,
+                ],
+                'config' => [
+                    'fields' => [
+                        'title' => [
+                            'type' => 'text',
+                            'label' => $module->l('Block title'),
+                            'default' => $module->l('Slot machine'),
+                        ],
+                        'instructions' => [
+                            'type' => 'editor',
+                            'label' => $module->l('Instructions text'),
+                            'default' => $module->l('Press the button to spin the reels and try your luck!'),
+                        ],
+                        'spin_button_label' => [
+                            'type' => 'text',
+                            'label' => $module->l('Spin button label'),
+                            'default' => $module->l('Spin the reels'),
+                        ],
+                        'result_title' => [
+                            'type' => 'text',
+                            'label' => $module->l('Result area title'),
+                            'default' => $module->l('Result'),
+                        ],
+                        'require_login' => [
+                            'type' => 'checkbox',
+                            'label' => $module->l('Require customers to be logged in to play'),
+                            'default' => true,
+                        ],
+                        'login_message' => [
+                            'type' => 'editor',
+                            'label' => $module->l('Message displayed when login is required'),
+                            'default' => $module->l('Log in to spin the slot machine!'),
+                        ],
+                        'start_date' => [
+                            'type' => 'text',
+                            'label' => $module->l('Game start date (YYYY-MM-DD HH:MM:SS)'),
+                            'default' => '',
+                        ],
+                        'end_date' => [
+                            'type' => 'text',
+                            'label' => $module->l('Game end date (YYYY-MM-DD HH:MM:SS)'),
+                            'default' => '',
+                        ],
+                        'pre_start_message' => [
+                            'type' => 'editor',
+                            'label' => $module->l('Message before the game starts'),
+                            'default' => '',
+                        ],
+                        'post_end_message' => [
+                            'type' => 'editor',
+                            'label' => $module->l('Message after the game ends'),
+                            'default' => '',
+                        ],
+                        'winning_combinations' => [
+                            'type' => 'textarea',
+                            'label' => $module->l('Winning combinations (JSON array)'),
+                            'default' => '[]',
+                        ],
+                        'default_coupon_name' => [
+                            'type' => 'text',
+                            'label' => $module->l('Default coupon name'),
+                            'default' => $module->l('Slot machine reward'),
+                        ],
+                        'default_coupon_prefix' => [
+                            'type' => 'text',
+                            'label' => $module->l('Default coupon code prefix'),
+                            'default' => 'SLOT',
+                        ],
+                        'default_coupon_validity' => [
+                            'type' => 'number',
+                            'label' => $module->l('Default coupon validity in days'),
+                            'default' => 30,
+                        ],
+                        'default_coupon_type' => [
+                            'type' => 'select',
+                            'label' => $module->l('Default discount type'),
+                            'default' => 'percent',
+                            'choices' => [
+                                'percent' => $module->l('Percent'),
+                                'amount' => $module->l('Amount'),
+                            ],
+                        ],
+                        'default_max_winners' => [
+                            'type' => 'number',
+                            'label' => $module->l('Maximum overall winners (0 for unlimited)'),
+                            'default' => 0,
+                        ],
+                        'padding_left' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding left (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_right' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding right (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_top' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding top (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'padding_bottom' => [
+                            'type' => 'text',
+                            'label' => $module->l('Padding bottom (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_left' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin left (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_right' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin right (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_top' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin top (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                        'margin_bottom' => [
+                            'type' => 'text',
+                            'label' => $module->l('Margin bottom (Please specify the unit of measurement)'),
+                            'default' => '',
+                        ],
+                    ],
+                ],
+                'repeater' => [
+                    'name' => 'Symbol',
+                    'nameFrom' => 'label',
+                    'groups' => [
+                        'symbol_key' => [
+                            'type' => 'text',
+                            'label' => $module->l('Internal symbol key'),
+                            'default' => '',
+                        ],
+                        'label' => [
+                            'type' => 'text',
+                            'label' => $module->l('Display label'),
+                            'default' => '',
+                        ],
+                        'probability' => [
+                            'type' => 'text',
+                            'label' => $module->l('Probability'),
+                            'default' => '1',
+                        ],
+                        'image' => [
+                            'type' => 'fileupload',
+                            'label' => $module->l('Symbol image'),
+                            'default' => [
+                                'url' => '',
+                            ],
+                        ],
+                        'alt_text' => [
+                            'type' => 'text',
+                            'label' => $module->l('Image alternative text'),
+                            'default' => '',
+                        ],
+                        'text' => [
+                            'type' => 'editor',
+                            'label' => $module->l('Accessible description'),
+                            'default' => '',
                         ],
                     ],
                 ],
