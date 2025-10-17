394af0b1f29ecd3c733f9f2380ee9f72fd6c7463
Cyssoo <cyrilchalamon@gmail.com>
2025-10-17 12:30:04 +0200

    lvl up 8.0.4

--- everblock.php
+++ everblock.php
@@ -65,7 +65,7 @@ class Everblock extends Module
     {
         $this->name = 'everblock';
         $this->tab = 'front_office_features';
-        $this->version = '8.0.3';
+        $this->version = '8.0.4';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
