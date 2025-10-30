0f94dfe9b677593cd3ae9b5402b5b74c347d4e92
Cyssoo <cyrilchalamon@gmail.com>
2025-10-30 08:24:19 +0100

    Bump version to 8.0.6 in everblock.php
    Updated the module version from 8.0.5 to 8.0.6 to reflect new changes or improvements.

--- everblock.php
+++ everblock.php
@@ -67,7 +67,7 @@ class Everblock extends Module
     {
         $this->name = 'everblock';
         $this->tab = 'front_office_features';
-        $this->version = '8.0.5';
+        $this->version = '8.0.6';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
