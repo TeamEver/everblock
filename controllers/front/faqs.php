6d9f2628dd70a7250ae2d78a8888fbf9d93c471a
Cyssoo <cyrilchalamon@gmail.com>
2025-12-09 17:23:46 +0100

    Add front FAQ tag listing with configurable routes

---
+++ controllers/front/faqs.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * 2019-2025 Team Ever
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ *  @author    Team Ever <https://www.team-ever.com/>
+ *  @copyright 2019-2025 Team Ever
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+require_once _PS_MODULE_DIR_ . 'everblock/models/EverblockFaq.php';
+
+class EverblockFaqsModuleFrontController extends ModuleFrontController
+{
+    /** @var string */
+    protected $tagName = '';
+
+    public function init()
+    {
+        parent::init();
+        $this->tagName = (string) Tools::getValue('tag', '');
+    }
+
+    public function initContent()
+    {
+        parent::initContent();
+
+        $tagName = trim($this->tagName);
+        if ($tagName === '') {
+            Tools::redirect($this->context->link->getPageLink('index'));
+        }
+
+        $itemsPerPage = (int) Configuration::get('EVERBLOCK_FAQ_PER_PAGE');
+        if ($itemsPerPage <= 0) {
+            $itemsPerPage = 10;
+        }
+
+        $currentPage = max(1, (int) Tools::getValue('page', 1));
+        $shopId = (int) $this->context->shop->id;
+        $langId = (int) $this->context->language->id;
+
+        $totalItems = EverblockFaq::countActiveByTagName($shopId, $tagName);
+        $totalPages = $totalItems > 0 ? (int) ceil($totalItems / $itemsPerPage) : 0;
+        if ($totalPages > 0 && $currentPage > $totalPages) {
+            $currentPage = $totalPages;
+        }
+
+        $faqs = EverblockFaq::getFaqByTagNamePaginated(
+            $shopId,
+            $langId,
+            $tagName,
+            $currentPage,
+            $itemsPerPage
+        );
+
+        $this->context->smarty->assign([
+            'everblock_tag_name' => $tagName,
+            'everblock_faqs' => $faqs,
+            'everblock_pagination' => $this->buildPagination(
+                $currentPage,
+                $totalPages,
+                $itemsPerPage,
+                $totalItems
+            ),
+            'everblock_structured_data' => $this->buildStructuredData($faqs),
+        ]);
+
+        $this->setTemplate('module:everblock/views/templates/front/faqs.tpl');
+
+        $this->setTemplateMeta(
+            $this->trans('FAQ', [], 'Modules.Everblock.Front') . ' - ' . $tagName,
+            $this->trans('Frequently asked questions grouped by tag.', [], 'Modules.Everblock.Front')
+        );
+    }
+
+    public function getBreadcrumbLinks()
+    {
+        $breadcrumb = parent::getBreadcrumbLinks();
+
+        $breadcrumb['links'][] = [
+            'title' => $this->trans('FAQ', [], 'Modules.Everblock.Front'),
+            'url' => '',
+        ];
+
+        if ($this->tagName !== '') {
+            $breadcrumb['links'][] = [
+                'title' => $this->tagName,
+                'url' => $this->getCanonicalURL(),
+            ];
+        }
+
+        return $breadcrumb;
+    }
+
+    protected function buildPagination(int $currentPage, int $totalPages, int $itemsPerPage, int $totalItems): array
+    {
+        $baseLink = $this->context->link->getModuleLink($this->module->name, 'faqs', [
+            'tag' => $this->tagName,
+        ]);
+        $pages = [];
+
+        for ($page = 1; $page <= $totalPages; ++$page) {
+            $pages[] = [
+                'number' => $page,
+                'link' => $page === 1
+                    ? $baseLink
+                    : $this->context->link->getModuleLink($this->module->name, 'faqs', [
+                        'tag' => $this->tagName,
+                        'page' => $page,
+                    ]),
+                'active' => $page === $currentPage,
+            ];
+        }
+
+        return [
+            'current' => $currentPage,
+            'total_pages' => $totalPages,
+            'items_per_page' => $itemsPerPage,
+            'total_items' => $totalItems,
+            'has_previous' => $currentPage > 1,
+            'has_next' => $currentPage < $totalPages,
+            'previous_link' => $currentPage > 1
+                ? ($currentPage - 1 === 1
+                    ? $baseLink
+                    : $this->context->link->getModuleLink($this->module->name, 'faqs', [
+                        'tag' => $this->tagName,
+                        'page' => $currentPage - 1,
+                    ]))
+                : $baseLink,
+            'next_link' => $currentPage < $totalPages
+                ? $this->context->link->getModuleLink($this->module->name, 'faqs', [
+                    'tag' => $this->tagName,
+                    'page' => $currentPage + 1,
+                ])
+                : $baseLink,
+            'pages' => $pages,
+        ];
+    }
+
+    protected function buildStructuredData(array $faqs): array
+    {
+        if (empty($faqs)) {
+            return [];
+        }
+
+        $entities = [];
+        foreach ($faqs as $faq) {
+            $entities[] = [
+                '@type' => 'Question',
+                'name' => $faq->title,
+                'acceptedAnswer' => [
+                    '@type' => 'Answer',
+                    'text' => strip_tags((string) $faq->content),
+                ],
+            ];
+        }
+
+        return [
+            '@context' => 'https://schema.org',
+            '@type' => 'FAQPage',
+            'name' => $this->trans('FAQ', [], 'Modules.Everblock.Front') . ' - ' . $this->tagName,
+            'mainEntity' => $entities,
+        ];
+    }
+
+    protected function setTemplateMeta(string $title, string $description): void
+    {
+        $this->meta_title = $title;
+        $this->meta_description = $description;
+        $this->context->smarty->assign([
+            'meta_title' => $title,
+            'meta_description' => $description,
+            'meta_robots' => 'index,follow',
+            'canonical' => $this->getCanonicalURL(),
+        ]);
+
+        $page = $this->context->controller->getTemplateVarPage();
+        $page['meta']['title'] = $title;
+        $page['meta']['description'] = $description;
+        $page['meta']['robots'] = 'index,follow';
+        $page['canonical'] = $this->getCanonicalURL();
+
+        $this->context->smarty->assign('page', $page);
+    }
+
+    public function getCanonicalURL()
+    {
+        return $this->context->link->getModuleLink($this->module->name, 'faqs', [
+            'tag' => $this->tagName,
+        ]);
+    }
+}
---
+++ views/templates/front/faqs.tpl
@@ -0,0 +1,49 @@
+{extends file='page.tpl'}
+
+{block name='page_title'}
+  {$smarty.block.parent}
+{/block}
+
+{block name='page_content'}
+  <section class="everblock-faqs-list">
+    <header class="mb-4">
+      <h1 class="h2 mb-2">{l s='FAQ' mod='everblock' d='Modules.Everblock.Front'} - {$everblock_tag_name|escape:'htmlall':'UTF-8'}</h1>
+      <p class="text-muted mb-0">{l s='All frequently asked questions grouped by this tag.' mod='everblock' d='Modules.Everblock.Front'}</p>
+    </header>
+
+    {if $everblock_faqs|@count}
+      {assign var='everFaqs' value=$everblock_faqs}
+      {include file='module:everblock/views/templates/hook/faq.tpl'}
+
+      {if isset($everblock_pagination.total_pages) && $everblock_pagination.total_pages > 1}
+        <nav class="mt-4" aria-label="{l s='Pagination' mod='everblock' d='Modules.Everblock.Front'}">
+          <ul class="pagination justify-content-center">
+            <li class="page-item{if !$everblock_pagination.has_previous} disabled{/if}">
+              <a class="page-link" href="{if $everblock_pagination.has_previous}{$everblock_pagination.previous_link|escape:'htmlall':'UTF-8'}{else}#{/if}" aria-label="{l s='Previous' mod='everblock' d='Modules.Everblock.Front'}">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            {foreach from=$everblock_pagination.pages item=paginationPage}
+              <li class="page-item{if $paginationPage.active} active{/if}">
+                <a class="page-link" href="{$paginationPage.link|escape:'htmlall':'UTF-8'}">{$paginationPage.number}</a>
+              </li>
+            {/foreach}
+            <li class="page-item{if !$everblock_pagination.has_next} disabled{/if}">
+              <a class="page-link" href="{if $everblock_pagination.has_next}{$everblock_pagination.next_link|escape:'htmlall':'UTF-8'}{else}#{/if}" aria-label="{l s='Next' mod='everblock' d='Modules.Everblock.Front'}">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      {/if}
+    {else}
+      <p class="alert alert-info">{l s='No FAQ available for this tag.' mod='everblock' d='Modules.Everblock.Front'}</p>
+    {/if}
+  </section>
+
+  {if !empty($everblock_structured_data)}
+    <script type="application/ld+json">
+      {$everblock_structured_data|json_encode:$smarty.const.JSON_UNESCAPED_SLASHES|replace:'\/':'/' nofilter}
+    </script>
+  {/if}
+{/block}
