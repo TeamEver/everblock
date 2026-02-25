599c04d59a0f6aabb60c444588857eaefe49d751
Cyssoo <cyrilchalamon@gmail.com>
2026-02-25 17:17:09 +0100

    Revert "Add declarative Instagram block for page builder"
    This reverts commit 2d358711dc04d894cfdf5aa6682bc7215c817da0.

--- everblock.php
+++ everblock.php
@@ -857,7 +857,6 @@ class Everblock extends Module
     {
         $defaultLogo = 'modules/' . $this->name . '/views/img/login.svg';
         $loginTemplate = 'views/templates/front/blocks/everblock_login.tpl';
-        $instagramTemplate = 'views/templates/hook/instagram.tpl';
 
         return [
             [
@@ -879,17 +878,6 @@ class Everblock extends Module
                             'default' => $this->l('Login'),
                         ],
                     ],
-                ],
-            ],
-            [
-                'name' => $this->l('Instagram feed'),
-                'description' => $this->l('Display Instagram feed block'),
-                'code' => 'everblock_instagram',
-                'tab' => 'general',
-                'icon_path' => $defaultLogo,
-                'need_reload' => true,
-                'templates' => [
-                    'default' => $instagramTemplate,
                 ],
             ],
         ];
