076f027636d01ccd6249b0cbe793881fa3fa2a00
Cyssoo <cyrilchalamon@gmail.com>
2026-08-19 13:51:41 +0200

    Bump module version to 9.0.6
    Update the Everblock module version from 9.0.5 to 9.0.6 to prepare the release.

--- everblock.php
+++ everblock.php
@@ -117,7 +117,7 @@ class Everblock extends Module
     {
         $this->name = 'everblock';
         $this->tab = 'front_office_features';
-        $this->version = '9.0.5';
+        $this->version = '9.0.6';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
